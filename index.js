f2ca2be6fba67e8c94ac3b8f6d6cac0eb798fc43
chathuraHarshanga <chathura749@gmail.com>
2024-05-13 11:35:29 +0530

    port change

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 9090;
 
 // Endpoint to get zodiac sign based on birthday
 app.get('/zodiac', (req, res) => {
